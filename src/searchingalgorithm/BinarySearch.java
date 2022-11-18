3b66cc81e44f97bb28f6e7704c92fad0f5b3b4ef
chandan kumar <chandankumarrana1898@gmail.com>
2022-11-19 04:46:59 +0530

    Binary search and problems

---
+++ src/searchingalgorithm/BinarySearch.java
@@ -0,0 +1,45 @@
+package searchingalgorithm;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 9, 11, 12, 14, 20, 36, 48};
+        int target = 36;
+        System.out.println(binarySearch(arr, target));
+    }
+
+    static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+//        check if the array is sorted in ascending order or descending order
+        boolean isAsc = arr[start] < arr[end];
+
+        while (start <= end) {
+//            find the middle element
+//            int mid = (start + end) / 2; // There might the possibility where start and end exceed the int range.
+            int mid = start + (end - start) / 2;
+
+            if (target == arr[mid]) {
+                return mid;
+            }
+
+            if (isAsc) {
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+
+        }
+        return -1;
+    }
+
+
+}
