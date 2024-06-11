1b4bfcc119fe9ac0c84feb17847e9d2f87105668
Chandan Kumar <chandan.k@inspironlabs.com>
2024-06-11 09:08:00 +0530

    https://leetcode.com/problems/two-sum : Two Sum Solution

--- src/leetcode/TwoSum.java
+++ src/leetcode/TwoSum.java
@@ -1,32 +1,40 @@
 package leetcode;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
-    private static int[] targetSumIndices(int[] nums, int target) {
-        if(nums.length == 0) {
-            return new int[] {-1, -1};
-        }
-        int len = nums.length - 1;
-        int next = 0;
-        int i=1;
-        while(i <= len) {
-            if(nums[i] + nums[next] == target) {
-                return new int[] {i, next};
+
+
+    private static int[] twoSumBruitForce(int[] numbers, int target) {
+        if (numbers.length == 0) return new int[]{};
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] == target) {
+                    return new int[]{i, j};
+                }
             }
-            if(i == len) {
-                next++;
-                i++;
-            }
-            i++;
         }
-        return new int[] {-1, -1};
+        return new int[]{};
+    }
+
+    private static int[] twoSum(int[] numbers, int target) {
+        Map<Integer, Integer> cacheMap = new HashMap<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (cacheMap.containsKey(target - numbers[i])) {
+                return new int[]{cacheMap.get(target - numbers[i]), i};
+            }
+            cacheMap.put(numbers[i], i);
+        }
+        return new int[]{};
     }
 
     public static void main(String[] args) {
-        int[] nums = {3,2,4};
-        int target = 6;
-        int[] res = targetSumIndices(nums, target);
+        int[] numbers = {2, 5, 5, 11};
+        int target = 10;
+        int[] res = twoSum(numbers, target);
         System.out.println(Arrays.toString(res));
     }
 }
